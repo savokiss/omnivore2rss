41c342f7aecc45b2a498cf820759340eea1c7d37
savokiss <jaynaruto@qq.com>
2024-03-26 19:18:27 +0800

    fix: close #1

--- src/utils.ts
+++ src/utils.ts
@@ -13,13 +13,14 @@ const getImageType = (imageUrl: string) => {
 const createRSSItem = (article: any) => {
   const node = article['node']
   const title = sanitizeXML(node['title'])
+  const link = sanitizeXML(node['url'])
   const content = sanitizeXML(node['content'])
   const author = node['author'] ? `<author>author@omnivore.app (${node['author']})</author>` : ''
   const imageType = node['image'] ? getImageType(node['image']) : null
 
   return `<item>
       <title>${title}</title>
-      <link>${node['url']}</link>
+      <link>${link}</link>
       <description><![CDATA[${content}]]></description>
       <pubDate>${new Date(node['createdAt']).toUTCString()}</pubDate>
       <guid isPermaLink="false">${node['id']}</guid>
@@ -30,6 +31,7 @@ const createRSSItem = (article: any) => {
 
 export const convertArticlesToRSS = (articles: any[], reqUrl: string) => {
   const items = articles.map(createRSSItem).join('')
+  const requestUrl = sanitizeXML(reqUrl)
 
   return `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
@@ -37,7 +39,7 @@ export const convertArticlesToRSS = (articles: any[], reqUrl: string) => {
       <title>Omnivore</title>
       <link>https://omnivore.app</link>
       <description>Articles from Omnivore</description>
-      <atom:link href="${reqUrl}" rel="self" type="application/rss+xml" />
+      <atom:link href="${requestUrl}" rel="self" type="application/rss+xml" />
       ${items}
   </channel>
 </rss>`
